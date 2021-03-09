ccba603bcd397c6d89e94416131640af62dc00d7
Dark-Ghost-Byte <Liadeochavez@gmail.com>
2021-03-09 13:39:21 -0700

    Actividades
    Actividad 1 y 2.

---
+++ Unidad_1/Hola_Mundo.java
@@ -0,0 +1,12 @@
+public class Hola_Mundo {
+    static int edad = 18;
+    int y = 0;
+
+    public static void main(String[] args) {
+        //Primer comentario
+        System.out.println ("Has vivido "+ (edad*365)+ " dias.");
+
+    }
+    //Segundo comentario
+
+}
---
+++ Unidad_1/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        //Primer comentario
+        short x1 = 1;
+        int x2 = 6;
+        short x3 = 12;
+        boolean entro = false;
+        String texto = "HOLA MUNDO";
+
+        int numero1 = 10;
+        int numero2 = 6;
+        int res = numero2++*2;
+
+        System.out.println (numero2++);
+        System.out.println (numero2);
+
+
+    }
+    //Segundo comentario
+}
