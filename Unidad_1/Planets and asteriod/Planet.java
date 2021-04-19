9cd357e302fd55e7481d4f5bbaa018e519643c64
Dark-Ghost-Byte <Liadeochavez@gmail.com>
2021-04-19 13:01:12 -0600

    Create Planet.java

---
+++ Unidad_1/Planets and asteriod/Planet.java
@@ -0,0 +1,20 @@
+public class Planet {
+    private String name;
+    private double sun_distance;
+    private String composition;
+    private int orbit_days;
+    public double mass;
+
+    //CONSTRUCTOR
+    public Planet (String nombre, double d, String c, int od, double m ) {
+
+        this.name = nombre;
+        this.sun_distance = d;
+        this.composition = c;
+        this.orbit_days = od;
+        this.mass = m;
+
+    }
+
+}
+
