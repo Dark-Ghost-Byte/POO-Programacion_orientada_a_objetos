3ea1e557b76b87b9219f06c8fe5a9fb4fc83aa02
Dark-Ghost-Byte <Liadeochavez@gmail.com>
2021-06-09 13:57:30 -0600

    Métodos y Clases Abstractas

---
+++ Unidad_1/La actividad 15/Principal.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Principal {
+    public static void main(String[] args) {
+        Circulo c = new Circulo(Color.RED, 25);
+        System.out.println(c.calcularArea());
+    }
+}
+
